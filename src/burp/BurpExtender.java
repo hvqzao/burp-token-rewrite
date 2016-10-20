f979be9dd7bca3c6c375b4ef03b8cff900c083f9
hvqzao <hvqzao@gmail.com>
2016-10-20 19:21:30 +0200

    Initial repo

---
+++ src/burp/BurpExtender.java
@@ -0,0 +1,10 @@
+package burp;
+
+public class BurpExtender implements IBurpExtender
+{
+    @Override
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks)
+    {
+        // your extension code here
+    }
+}
